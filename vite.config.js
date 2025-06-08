1a40114ccd21231403ff12a379312159d8d74bc8
u2re-dev <c24b@u2re.ru>
2025-06-08 15:41:32 +0700

    use import maps

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,35 @@ export const __dirname = resolve(import.meta.dirname, "./");
 export default {
     plugins,
     worker,
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src/"),
+            "@mods": resolve(__dirname, "./src/$blit$/"),
+            "@ext": resolve(__dirname, "./src/$ext$/"),
+            "@blit": resolve(__dirname, "./src/$blit$/"),
+            "@scss": resolve(__dirname, "./src/$scss$/"),
+            "@temp": resolve(__dirname, "./src/$temp$/"),
+            "@service": resolve(__dirname, "./src/$service$/"),
+            "/assets/": resolve(__dirname, "./assets/"),
+            "/frontend/": resolve(__dirname, "./frontend/"),
+            "/plugins/": resolve(__dirname, "./plugins/"),
+            "u2re/": resolve(__dirname, '/externals/modules/'),
+            'u2re-src/': resolve(__dirname, '../'),
+            "u2re/cdnImport": resolve(__dirname, '../cdnImport.mjs'),
+            "u2re/dom": resolve(__dirname, "../dom.ts/src/index.ts"),
+            "u2re/lure": resolve(__dirname, "../BLU.E/src/index.ts"),
+            "u2re/object": resolve(__dirname, "../object.ts/src/index.ts"),
+            "u2re/uniform": resolve(__dirname, "../uniform.ts/src/index.ts"),
+            "u2re/theme": resolve(__dirname, "../theme.core/src/index.ts"),
+        },
+    },
     server: {
         port: 5173,
         open: false,
         origin: "/",
+        fs: {
+            allow: ['..', resolve(__dirname, '../') ]
+        },
     },
     build: {
         chunkSizeWarningLimit: 1600,
